e6269c8507462eef2610ea5a5ffaabea3eabf45b
SAYAN MAITY <sayanmaity007@gmail.com>
2020-06-15 21:18:48 +0530

    route integrate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 import './styles/tailwind.css'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+import RoomSection from './components/roomSection/RoomSection.jsx'
+import Home from './components/Home/Home';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <p className="text-blue-400 font-semibold">Start your project</p>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/room-section' component={RoomSection} />
+      </Switch>
     </div>
+
+    </Router>
   );
 }
 
---
+++ src/components/roomSection/RoomSection.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RoomSection extends Component {
+  render() {
+    return (
+      <div>
+        <p>roomSection</p>
+      </div>
+    )
+  }
+}
